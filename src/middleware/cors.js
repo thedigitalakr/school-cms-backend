6256a16977176b658f111e6bb4f33ff83bbb93db
Minor Project <thedigitalakr@gmail.com>
2025-11-03 00:50:09 +0530

    Initial commit - backend setup updatee

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -14,10 +14,7 @@
  */
 
 const DEFAULT_DEV_ORIGINS = [
-  "http://localhost:5173",
-  "http://127.0.0.1:5173",
-  "http://localhost:3000",
-  "http://127.0.0.1:3000"
+  "http://ghs50.in/"
 ];
 
 function compilePatterns(rawList) {
